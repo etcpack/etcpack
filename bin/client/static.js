96a892a295d9c3ca5264c14329045b12b36eeb57
你好2007 <2501482523@qq.com>
2021-12-22 16:48:00 +0800

    修复watch超过11次错误问题

--- bin/client/static.js
+++ bin/client/static.js
@@ -1,5 +1,4 @@
 var ws = new WebSocket('ws://localhost:20001/refresh-page');
-var flag = false;
 
 // 连接成功
 ws.addEventListener('open', function (event) {
@@ -8,7 +7,6 @@ ws.addEventListener('open', function (event) {
 
 // 监听来自服务器的数据
 ws.addEventListener('message', function (event) {
-    if (!flag) { flag = true; return; }
 
     // 如果把该方法的参数设置为 true，那么无论文档的最后修改日期是什么，
     // 它都会绕过缓存，从服务器上重新下载该文档
