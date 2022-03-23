33b168cfd2d28678e4fd7600e925c2af9ffdcf6f
你好2007 <2501482523@qq.com>
2022-03-23 10:47:03 +0800

    针对加载失败的文件进行提示而不是直接奔溃

--- etcpack/tool/getMainUrl.js
+++ etcpack/tool/getMainUrl.js
@@ -2,9 +2,17 @@ const nodejs = require('@hai2007/nodejs');
 
 module.exports = function (filepath) {
 
-    let packageUrl = nodejs.fullPath('./package.json', filepath);
-    let packageContent = require(packageUrl);
+    let packageUrl = nodejs.fullPath('./package.json', filepath), packageContent;
 
-    // 先直接取main，后期扩展语法
-    return nodejs.fullPath(packageContent.main, filepath);
+    // 为了解决map等一些文件加载失败后服务器奔溃问题
+    // by 你好2007 2022年3月23日 南京
+    try {
+        packageContent = require(packageUrl);
+    } catch (e) {
+        nodejs.error("Loading failed：" + filepath);
+    }
+
+    // 先取module,如果不存在取main
+    // 后期扩展语法
+    return packageContent ? nodejs.fullPath("module" in packageContent ? packageContent.module : packageContent.main, filepath) : filepath;
 };
